d5bc5dd6af9f7f6913960313f6ea26d387df9628
Zywilla Fechner <zwfdane@gmail.com>
2022-06-19 18:29:34 +0200

    Initial commit

---
+++ lista_zakupow.py
@@ -0,0 +1,18 @@
+print("Lista zakupów")
+shopping_list_dict = {
+    "Piekarnia": ["Chleb","Pączek","Bułki"],
+    "Warzywniak": ["Marchew","Seler","Rukola"]
+}
+Leng = 0
+for shop,items in shopping_list_dict.items():
+    Leng = Leng + len(items)
+    print(f"Idę do {shop.upper()} i kupuję")
+    for product in items:
+        print(f"{product.upper()} ")
+    
+
+print(f"W sumie kupuję {Leng} produktów.")
+
+# Można zmodyfikować słownik na upper i powtórzyć pętle w wersji dla upper.
+# Fajne zadanie na listy. Jest kilka możliwości. 
+# Próba
